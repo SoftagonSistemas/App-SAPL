525277030a5395dae306fa451e921ba20fa4aabb
Hermes Alves <hermes.alves@araripina.com.br>
2024-06-25 10:07:45 -0300

    loop infinity

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
-import { IonContent, IonPage } from '@ionic/vue'
+import { IonButton, IonContent, IonPage } from '@ionic/vue'
 import { Browser } from '@capacitor/browser'
 import { App } from '@capacitor/app'
-import { onMounted } from 'vue'
+import { onMounted, ref } from 'vue'
+
+const showExitButton = ref(false)
 
 onMounted(async () => {
   await App.addListener('backButton', ({ canGoBack }) => {
@@ -16,13 +18,15 @@ onMounted(async () => {
   const month = currentDate.getMonth() + 1
   const day = currentDate.getDate()
 
-  const sapl = '`https://sapl.salitre.ce.leg.br'
+  const sapl = 'https://sapl.salitre.ce.leg.br'
   const url = `${sapl}/sessao/pesquisar-sessao?data_inicio__year=${year}&data_inicio__month=${month}&data_inicio__day=${day}&tipo=&salvar=Pesquisar`
 
   await Browser.open({ url })
-  Browser.addListener('browserFinished', () => {
-    App.exitApp()
-  })
+
+  // Adicione um atraso antes de mostrar o botão de saída
+  setTimeout(() => {
+    showExitButton.value = true
+  }, 5000) // 5 segundos
 })
 </script>
 
@@ -30,6 +34,9 @@ onMounted(async () => {
   <IonPage>
     <IonContent>
       <p>Softagon Sistemas</p>
+      <IonButton v-if="showExitButton" @click="App.exitApp">
+        Sair do Aplicativo
+      </IonButton>
     </IonContent>
   </IonPage>
 </template>
