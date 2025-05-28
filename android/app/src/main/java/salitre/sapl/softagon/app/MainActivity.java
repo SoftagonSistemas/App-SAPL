9815738a029f0be9d8f0df8c77e9ab06581db51d
Hermes Alves <hermes@softagon.com.br>
2025-05-27 22:15:52 -0300

    Refactor code structure and remove redundant changes

---
+++ android/app/src/main/java/salitre/sapl/softagon/app/MainActivity.java
@@ -0,0 +1,17 @@
+package generico.sapl.softagon.app;
+
+import android.os.Bundle;
+import android.view.View;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    getWindow().getDecorView().setSystemUiVisibility(
+      View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+      | View.SYSTEM_UI_FLAG_FULLSCREEN
+      | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+    );
+  }
+}
